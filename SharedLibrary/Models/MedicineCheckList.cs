0940f179cd463f2588e1627ecc2173bff80fb548
kelllly322 <kellylan322@gmail.com>
2024-04-30 15:03:28 +0800

    Added: MedicineCheckList, ManageMedicineCheckList, MedicineCheckList.cs(Database)

---
+++ SharedLibrary/Models/MedicineCheckList.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SharedLibrary.Models
+{
+   public class MedicineCheckList
+    {
+        [Required]
+        public int No { get; set; }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string MedicineName { get; set; } = string.Empty;
+        public string ImageUrl { get; set; }
+        [Required]
+        public string Instruction { get; set; } = string.Empty;
+        [Required]
+        public int EachDosage { get; set; }
+        [Required]
+        public string UnitOfEachDosage { get; set; }
+        public bool DidOrDidntTakeMedicine { get; set; }
+        public string SymptomDescription { get; set; }
+        [Required]
+        public string Operator { get; set; }
+        [Required]
+        public DateTime OperationTime { get; set; }
+    }
+}
